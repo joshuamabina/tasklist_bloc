bcdd833a4f3b108564d038d74ce23a482a2641a3
Joshua Mabina <mabinajoshua@gmail.com>
2023-12-30 14:44:21 +0300

    wip: describe an interface for an api that gets us a list of tasks

---
+++ packages/tasklist_api/test/tasklist_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:tasklist_api/tasklist_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
